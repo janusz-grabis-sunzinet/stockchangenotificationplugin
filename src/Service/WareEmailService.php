47c353b84c1788d0816dae3c9ee47fa082454061
Janusz Grabis <janusz.grabis@sunzinet.com>
2023-11-16 14:09:27 +0100

    Fixed di for repository

--- src/Service/WareEmailService.php
+++ src/Service/WareEmailService.php
@@ -2,10 +2,26 @@
 
 namespace StockChangeNotificationPlugin\Service;
 
-class WareEmailService {
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 
-    public function saveWareEmail(string $email) : void {
-        echo $email; exit;
+class WareEmailService
+{
+
+    protected EntityRepository $wareEmailRepository;
+
+    public function __construct(EntityRepository $wareEmailRepository)
+    {
+
+        $this->wareEmailRepository = $wareEmailRepository;
+
+    }
+
+    public function saveWareEmail(string $email): void
+    {
+
+        //TODO: do the saving here
+        echo $email;
+        exit;
 
     }
 }
